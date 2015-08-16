ad7e6a82dedd0c3352b39af3e2932c211274c9d5
Marco Antonio <marco@ifc-sombrio.edu.br>
2015-08-16 17:20:58 -0300

    Apresentação

--- README.md
+++ README.md
@@ -7,11 +7,12 @@ Biblioteca RTTY no Arduino
 - Biblioteca RTTY modificada
 - decodificação com minimodem
 
-```sh
+```
 minimodem --rx 50 --mark 2295 --space 2125 -8 
 ```
-##Links
-[RTTY Library for Arduino](http://www.timzaman.com/2011/04/radio-rtty-library-for-arduino/)
-[Arduino RTTY](http://emmanuelgranatello.blogspot.com.br/2012/07/arduino-rtty.html)
+>**Links
+> -[RTTY Library for Arduino](http://www.timzaman.com/2011/04/radio-rtty-library-for-arduino/)
+> -[Arduino RTTY](http://emmanuelgranatello.blogspot.com.br/2012/07/arduino-rtty.html)
+> -[Demonstração](https://youtu.be/SwSwrsqfsNM)
 
 
--- RTTY/RTTY.ino
+++ RTTY/RTTY.ino
@@ -7,26 +7,13 @@
 // http://emmanuelgranatello.blogspot.com.br/2012/07/arduino-rtty.html
 
 #include <RTTY.h>
- 
-//float tempC;
-//int tempPin = 0;
-//char buffer [50];
- 
 void setup()
 {
-  /*
-My Library RTTY accepts a single parameter in the
-constructor (output audio tone)
-  */
   RTTY.attach(8);
 }
  
 void loop()
 {
-  //tempC = analogRead(tempPin);
-  //tempC = (5.0 * tempC * 100.0)/1024.0;
- 
-  //RTTY.tx(" Remote Temperature Sensor ");
   RTTY.tx(" \n");
   RTTY.tx(" Enviando dados por RTTY \n");
   RTTY.tx("  _ ___ __  \n");
@@ -34,7 +21,4 @@ void loop()
   RTTY.tx(" | | _( (_  \n");
   RTTY.tx(" |_|_| \\__| \n");
   RTTY.tx("            by PU5NIX \n");
-  //dtostrf(tempC, 2, 2, buffer);
-  //RTTY.tx(buffer);
-  //RTTY.tx("c\n");
 }
